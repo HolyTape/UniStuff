0e1424ae9b1374f9c61404e38ed5d3dbb625555a
HolyTape <idennisendr@gmail.com>
2017-10-25 10:30:47 +0300

    Update masks.c

--- Lab#1/masks.c
+++ Lab#1/masks.c
@@ -46,4 +46,40 @@ int main()
                                 else if (a & c8) printf("8 ");
                                     else printf("no filled bits");
     return 0;
+}
+
+int main()
+{
+    char a = 17;
+    char c1 = 0x01,
+         c2 = 0x02,
+         c3 = 0x04,
+         c4 = 0x08,
+         c5 = 0x10,
+         c6 = 0x20,
+         c7 = 0x40,
+         c8 = 0x80;
+    printf("a = %d\nthe thing = ", a);
+
+    if (a & c1) printf("1 ");
+        else if (a & c2) printf("2, ");
+            else if (a & c3) printf("3, ");
+                else if (a & c4) printf("4, ");
+                    else
+                        {
+                            printf("no filled bits in first byte, ");
+                            if (a & c5) printf("1"); else printf("0");
+                            if (a & c6) printf("1"); else printf("0");
+                            if (a & c7) printf("1"); else printf("0");
+                            if (a & c8) printf("1"); else printf("0");
+                        }
+    if((a & c1) | (a & c2) | (a & c3) | (a & c4))
+    {
+        if (a & c5) printf("5 ");
+        if (a & c6) printf("6 ");
+        if (a & c7) printf("7 ");
+        if (a & c8) printf("8 ");
+    }
+
+    return 0;
 }*/
